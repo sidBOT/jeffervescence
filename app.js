7befa42dc59dfa2192a79c76fdc78bf1188d7ca1
Siddhant Patel <si12hant@gmail.com>
2017-06-08 11:57:47 -0400

    Save flicks in localstorage

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+
 const app = {
+  
   init(selectors) {
     this.flicks = []
     this.max = 0
@@ -20,11 +22,16 @@ const app = {
       name: f.flickName.value,
     }
     this.flicks.unshift(flick)
+    this.save()
     const listItem = this.renderListItem(flick)
     // this.list.appendChild(listItem)
     this.list.insertBefore(listItem, this.list.firstChild)
     ++ this.max
     f.flickName.value = ''
+  },
+
+  save() {
+    localStorage.setItem('flicks', JSON.stringify(this.flicks))
   },
 
   renderListItem(flick) {
@@ -33,12 +40,20 @@ const app = {
     item.querySelector('.flick-name').textContent = flick.name
     item.dataset.id = flick.id
 
-    item.querySelector('.button.remove').addEventListener('click', this.removeFlick)
+    item.querySelector('.button.remove').addEventListener('click', this.removeFlick.bind(this))
     return item
   },
   removeFlick(ev) {
    ev.target.closest('.flick').remove()
 
+   for(let i = 0; i < this.flicks.length;i++) {
+     const currentID = this.flicks[i].id.toString()
+    if(listItem.dataset.id === currentID) {
+      this.flicks.splice(i, 1)
+      break
+    }
+   }
+   this.save()
   },  
 }
 
